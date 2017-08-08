b6ad5cdd99a6c63a3aef64cc2f5c23fb6eb4dd7c
yutahonma <yuta.honma@rakus.co.jp>
2017-08-08 13:52:04 +0900

    練習用のコミットです

---
+++ src/main/java/jp/co/rakus/git_sample_honma2/Car.java
@@ -0,0 +1,21 @@
+package jp.co.rakus.git_sample_honma2;
+
+public class Car {
+	private int speed;
+	private int tire;
+	
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getTire() {
+		return tire;
+	}
+	public void setTire(int tire) {
+		this.tire = tire;
+	}
+	
+	
+}
